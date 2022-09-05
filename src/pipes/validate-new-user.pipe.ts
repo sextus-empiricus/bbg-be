7001296b8010f00a988ba186af72615617f6b734
Sextus Empiricus <8272331@gmail.com>
2022-09-05 06:50:18 +0200

    users.e2e-spec - test created;

--- src/pipes/validate-new-user.pipe.ts
+++ src/pipes/validate-new-user.pipe.ts
@@ -1,20 +1,29 @@
 import {
    ArgumentMetadata,
    ConflictException,
+   Inject,
    Injectable,
    PipeTransform,
 } from '@nestjs/common';
-import { CreateUserDto } from '../users/dto/create-user.dto';
+import { CreateUserDto } from '../users/dto';
+import { DataSource } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { hash } from 'bcrypt';
 
 @Injectable()
 export class ValidateNewUserPipe implements PipeTransform {
+   constructor(@Inject(DataSource) private readonly dataSource: DataSource) {}
+
    async transform(
       value: any,
       metadata: ArgumentMetadata,
    ): Promise<CreateUserDto> {
-      const targetUser = await User.findOne({ where: { email: value.email } });
+      const targetUser = await this.dataSource
+         .createQueryBuilder()
+         .select('user')
+         .from(User, 'user')
+         .where({ email: value.email  })
+         .getOne();
       if (targetUser) {
          throw new ConflictException('Provided email address is already used.');
       } else {
