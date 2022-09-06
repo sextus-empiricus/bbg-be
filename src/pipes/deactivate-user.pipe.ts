20f979cab3f19bb6ca711931d4c37929b0451393
Sextus Empiricus <8272331@gmail.com>
2022-09-06 10:20:06 +0200

    deactivate-user.pipe logic fixed, baseEntity => dataSource

--- src/pipes/deactivate-user.pipe.ts
+++ src/pipes/deactivate-user.pipe.ts
@@ -3,13 +3,25 @@ import {
    ArgumentMetadata,
    ConflictException,
    PipeTransform,
+   Inject,
 } from '@nestjs/common';
 import { User } from '../users/entities/user.entity';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class DeactivateUserPipe implements PipeTransform {
+   constructor(@Inject(DataSource) private dataSource: DataSource) {}
+
    async transform(value: string, metadata: ArgumentMetadata): Promise<void> {
-      const targetUser = await User.findOne({ where: { id: value } });
+      /*ℹCould use UsersService to get a user, but we are looking for `isActive` 
+      and the service returns filterd data.*/
+      const targetUser = await this.dataSource
+         .createQueryBuilder()
+         .select('user')
+         .from(User, 'user')
+         .where({id: value})
+         .getOne();
+
       if (!targetUser) {
          throw new ConflictException(`No user found matches provided id.`);
       } else if (targetUser.isActive === false) {
