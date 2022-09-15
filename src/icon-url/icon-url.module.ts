ab1346958a2151d29aa527cb19a6485a7634ac8d
Sextus Empiricus <8272331@gmail.com>
2022-09-15 10:08:40 +0200

    attach icon to newTradeDto functionality moved to IconUrlService;

--- src/icon-url/icon-url.module.ts
+++ src/icon-url/icon-url.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ExternalApisModule } from '../external-apis/external-apis.module';
 import { IconUrlService } from './icon-url.service';
 
 @Module({
+   imports: [ExternalApisModule],
    providers: [IconUrlService],
    exports: [IconUrlService],
 })
